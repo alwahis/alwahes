1206e137c5930b2415ab898de6f7dfd32a27b6a1
Mudhafar Hamid <mudhafarfarhan@gmail.com>
2025-06-09 12:19:09 +0200

    Fix MIME type issues and update Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   // Base URL for production
-  base: './',
+  base: '/',
   
   // Define global constants
   define: {
